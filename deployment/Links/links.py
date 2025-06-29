a14e851e4399d18df93b0d5f1e5f78c01851c6c6
juniorta <ttjrbiz@gmail.com>
2025-06-29 19:16:17 -0400

    me section

--- deployment/Links/links.py
+++ deployment/Links/links.py
@@ -4,8 +4,6 @@ from PIL import Image
 
 st_function.load_css()
 
-st.write("[![Star](https://img.shields.io/github/stars/dataprofessor/links.svg?logo=github&style=social)](https://gitHub.com/dataprofessor/links)")
-
 col1, col2, col3 = st.columns(3)
 col2.image(Image.open('deployment/Links/pic.png'),)
 
@@ -16,5 +14,8 @@ st.info('Undergraduate student researcher, Sports Analytics practitioner and IBM
 
 icon_size = 20
 
-st_function.st_button('linkedin', 'https://www.linkedin.com/in/juniorta/', 'Follow me on LinkedIn', icon_size)
-st_function.st_button('cup', 'https://www.paypal.me/juniortadiffo', 'Donate', icon_size)
+st_function.st_button('linkedin', 'https://www.linkedin.com/in/juniorta/', '  Connect on LinkedIn', icon_size)
+st_function.st_button('github', 'https://github.com/junior-ta', 'GitHub', 20)
+st_function.st_button('email', 'juniorta@buffalo.edu', 'Email Me', 20)
+st_function.st_button('instagram', 'https://www.instagram.com/tajr.r.r.r/', 'Instagram', 20)
+st_function.st_button('cup', 'https://www.paypal.me/juniortadiffo', '  Donate', icon_size)
---
+++ deployment/Project's_stack.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.markdown("<h1 style='text-align: center;'>Stack</h1>", unsafe_allow_html=True)
+st.markdown("Language: **Python**")
+st.markdown("<br>", unsafe_allow_html=True)
+st.markdown("""
+- **matplotlib**
+
+- **numpy**
+
+- **pandas**
+
+- **seaborn**
+
+- **statsmodels**
+
+- **streamlit**
+""")
